6121237b52674ffd902a09840756f1910be2063d
Polash Mahmud <polashmahmud@gmail.com>
2024-04-08 18:03:08 +0600

    fix home, dashboard controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function __invoke()
     {
         return inertia()->render('Home');
     }
